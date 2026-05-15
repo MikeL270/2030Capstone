20198096edb84902f1a056fb2a1d49774fade5c0
Michael Lance <mlance4@uwyo.edu>
2026-05-14 18:16:33 -0600

    remove annoying merge conflict text from files. Thanks for screwing up, git

--- airial_api/app/routers/schemas.py
+++ airial_api/app/routers/schemas.py
@@ -93,7 +93,6 @@ def create(body: createSchemaReq):
         abort(500)
 
     return schema.to_dict(), 201
-<<<<<<< HEAD
 
 
 # ---------------------------------------------------------------------------------------------------------------------------
@@ -116,5 +115,3 @@ def delete_schema(schema_id: str):
         abort(500)
 
     return "", 204
-=======
->>>>>>> 2f5c112b260d890ee9375bc2e3e048da9aaad6c9
